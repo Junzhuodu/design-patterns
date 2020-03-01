28b99a5f5d4a6f9becb4ce6abef2c66b49ecc732
Junzhuo Du <junzhuod@xsense.ai>
2020-03-01 11:26:39 -0800

    Create State Pattern
    Signed-off-by: Junzhuo Du <junzhuod@xsense.ai>

--- BehaviroalPatterns/state/State.cxx
+++ BehaviroalPatterns/state/State.cxx
@@ -5,3 +5,61 @@
  *
  */
 
+#include <iostream>
+
+class State {
+public:
+    virtual ~State() {}
+    
+    virtual void handle() = 0;
+};
+
+class ConcreteStateA : public State {
+    ~ConcreteStateA() {}
+    
+    void handle() {
+        std::cout << "State A handled." << std::endl;
+    }
+};
+
+class ConcreteStateB : public State {
+    ~ConcreteStateB() {}
+
+    void handle() {
+        std::cout << "State B handled." << std::endl;
+    }
+};
+
+class Context {
+public:
+    Context() : state() {}
+    ~Context() {
+        if (state) {
+            delete state;
+        }
+    }
+    
+    void setState(State* const s) {
+        if (state) {
+            delete state;
+        }
+        state = s;
+    }
+    
+    void request() {
+        state->handle();
+    }
+    
+private:
+    State* state;
+};
+
+int main() {
+    Context* context = new Context();
+    
+    context->setState(new ConcreteStateA());
+    context->request();
+    
+    context->setState(new ConcreteStateB());
+    context->request();
+}
