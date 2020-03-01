2711fde20351074392e99edd3d409d7cef374fc7
Junzhuo Du <junzhuod@xsense.ai>
2020-03-01 11:37:31 -0800

    Create Strategy Pattern
    Signed-off-by: Junzhuo Du <junzhuod@xsense.ai>

--- BehaviroalPatterns/strategy/Strategy.cxx
+++ BehaviroalPatterns/strategy/Strategy.cxx
@@ -5,3 +5,53 @@
  *
  */
 
+#include <iostream>
+
+class Strategy {
+public:
+    virtual ~Strategy() {}
+    virtual void algorithmInterface() = 0;
+};
+
+class ConcreteStrategyA : public Strategy {
+    ~ConcreteStrategyA() {}
+
+    void algorithmInterface() {
+        std::cout << "Concrete Strategy A" << std::endl;
+    }
+};
+
+class ConcreteStrategyB : public Strategy {
+    ~ConcreteStrategyB() {}
+
+    void algorithmInterface() {
+        std::cout << "Concrete Strategy B" << std::endl;
+    }
+};
+
+class ConcreteStrategyC : public Strategy {
+    ~ConcreteStrategyC() {}
+
+    void algorithmInterface() {
+        std::cout << "Concrete Strategy C" << std::endl;
+    }
+};
+
+class Context {
+public:
+    Context(Strategy* s) : strategy(s) {}
+    ~Context() {
+        delete strategy;
+    }
+
+    void contextInterface() {
+        strategy->algorithmInterface();
+    }
+private:
+    Strategy* strategy;
+};
+
+int main() {
+    Context context(new ConcreteStrategyA());
+    context.contextInterface();
+}
