fc2a8f5cb99a674918619bc3585815796e175411
henrytien <tianhangyu8886@gmail.com>
2022-11-23 03:04:36 +0000

    fix:segment core if not initialize the pointer

--- CreationalPatterns/prototype/Prototype.cxx
+++ CreationalPatterns/prototype/Prototype.cxx
@@ -9,7 +9,7 @@
 #include <string>
 
 class Prototype {
-public:
+ public:
   Prototype() { std::cout << "Prototpye" << std::endl; }
   virtual ~Prototype() {}
 
@@ -18,33 +18,40 @@ public:
 };
 
 class ConcreatePrototype1 : public Prototype {
-public:
+ public:
   ConcreatePrototype1() { std::cout << "ConcreatePrototype1" << std::endl; }
   ~ConcreatePrototype1() {}
 
   Prototype* clone() { return new ConcreatePrototype1; }
-  void checkPrototype() { std::cout << "Prototype1 has been created" << std::endl; }
+  void checkPrototype() {
+    std::cout << "Prototype1 has been created" << std::endl;
+  }
 };
 
 class ConcreatePrototype2 : public Prototype {
-public:
+ public:
   ConcreatePrototype2() { std::cout << "ConcreatePrototype2" << std::endl; }
   ~ConcreatePrototype2() {}
 
   Prototype* clone() { return new ConcreatePrototype2; }
-  void checkPrototype() { std::cout << "Prototype2 has been created" << std::endl; }
+  void checkPrototype() {
+    std::cout << "Prototype2 has been created" << std::endl;
+  }
 };
 
 class Client {
-public:
-  Client() { std::cout << "Client" << std::endl; }
+ public:
+  Client() {
+    prototype = NULL;
+    std::cout << "Client" << std::endl;
+  }
   ~Client() {
     if (prototype) {
       delete prototype;
     }
   }
 
-  void setPrototype(Prototype *p) {
+  void setPrototype(Prototype* p) {
     if (prototype) {
       delete prototype;
     }
@@ -57,18 +64,17 @@ public:
     return prototype->clone();
   }
 
-private:
-  Prototype *prototype;
+ private:
+  Prototype* prototype;
 };
 
 int main(int argc, char* argv[]) {
   Client client;
   client.setPrototype(new ConcreatePrototype1);
-  Prototype *p1 = client.clone();
+  Prototype* p1 = client.clone();
   p1->checkPrototype();
 
   client.setPrototype(new ConcreatePrototype2);
-  Prototype *p2 = client.clone();
+  Prototype* p2 = client.clone();
   p2->checkPrototype();
-
 }
